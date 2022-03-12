18fdfff14240d88a0be5575da6723f8d8261542c
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-03-12 11:31:51 -0600

    fetchrequest

--- homepage.js
+++ homepage.js
@@ -1,57 +1,31 @@
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
-var repoContainerEl = document.querySelector("#repos-container");
-var repoSearchTerm = document.querySelector("#repo-search-term")
 
-var formSubmitHandler = function(event) {
+var getUserRepos = function(user) {
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+     fetch(apiUrl).then(function(response) {
+         response.json().then(function(data) {
+             console.log(data);
+         });
+      });
+    };
+getUserRepos();
+
+
+var formSubmitHandler = function(event){
     event.preventDefault();
-    //get value form input element
+    //get value from input element
     var username = nameInputEl.value.trim();
-
     if (username) {
         getUserRepos(username);
-        nameInputEl.value ="";
-    } else {
-        alert("Please enter a GitHub username");
+        nameInputEl.value = "";
+    } else { 
+        alert("Pleae enter a GitHub username");
     }
-
-    console.log(event);
-};
-var getUserRepos = function(user) {
-    // format the github api url
-    var apiUrl = "https://api.github.com/users/" + user + "/repos";
-    //make a request to the url
-   response.json().then(function(data) {
-       displayRepos(data,user);
-   });
     
-};
-var displayRepos = function(repos, searchTerm) {
-    console.log(repos);
-    console.log(searchTerm);
-    //clear old content
-    repoContainerEl.textContent = "";
-    repoSearchTerm.textContent = searchTerm;
-    // loop over repos
-    for (var i = 0; i < repos.length; i++) {
-        //format repo  name
-        var repoName = repos[i].owner.login + "/" + repos[i].name;
-        // create a container for each repo
-        var repoEl = document.createElement("div");
-        repoEl.classList +"list-item flex-row justify-space-between align-center"
-        // create a span element to hold the repo name
-        var titleEl = document.createElement('span');
-        titleEl.textContent = repoName;
-        //append to container
-        repoEl.appendChild(titleEl);
-        // append container to the dom
-        repoContainerEl.appendChild(repoEl);
-    }
+    console.log(event);
 };
 
 /* fetch(apiUrl).then(function (response) { ... }); */
 userFormEl.addEventListener("submit", formSubmitHandler);
-
-
-
 
